7d0d8c2d12dea19fa1a67f0b5c748f3528bfd252
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-22 23:07:28 +0100

    Added order creation

--- shuffleMode.h
+++ shuffleMode.h
@@ -1,12 +1,15 @@
 #ifndef SHUFFLEMODE_H
 #define SHUFFLEMODE_H
 
-
 #include "mode.h"
 
 class ShuffleMode : public Mode {
+private:
+    int seed;
 public:
-    std::vector<unsigned int> getOrder() override;
+    ShuffleMode(int seed) : seed(seed) {}
+
+    std::vector<unsigned int> getOrder(std::vector<Playable *> list) override;
 };
 
 
