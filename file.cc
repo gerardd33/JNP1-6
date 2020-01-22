b9db543b7d199ca629942154cbd737c9bb6ea841
gerardd33 <gerardd@interia.pl>
2020-01-22 20:11:52 +0100

    implemented the file parser

--- file.cc
+++ file.cc
@@ -1,4 +1,6 @@
+#include <sstream>
 #include "file.h"
+
 
 std::string* File::getDataField(std::string dataFieldName) {
 	auto it = metadata.find(dataFieldName);
@@ -7,8 +9,24 @@ std::string* File::getDataField(std::string dataFieldName) {
 	else return nullptr;
 }
 
-// TODO: czy tutaj w argumencie powinna byc const referencja?
-File::File(std::string data) {
-	// TODO: parser
-	//  ...
+// TODO: czy tutaj w argumencie powinno byc const
+File::File(std::string& data) {
+	if (data.empty())
+		return;
+
+	std::string fieldName, fieldValue;
+	std::istringstream iss(data);
+
+	std::getline(iss, fieldName, '|');
+	metadata[TYPE_FIELD_NAME] = fieldName;
+
+	while (getline(iss, fieldName, ':')) {
+		// TODO: czy tutaj sie nie wywala przy pustej
+		//  koncowce lub innych cornerach, wyifuj
+
+		std::getline(iss, fieldValue, '|');
+		metadata[fieldName] = fieldValue;
+	}
+
+	metadata[CONTENT_FIELD_NAME] = fieldName;
 }
--- file.h
+++ file.h
@@ -4,6 +4,10 @@
 #include <string>
 #include <map>
 
+const std::string TYPE_FIELD_NAME = "|type";
+const std::string CONTENT_FIELD_NAME = "|content";
+
+
 class File {
 public:
 	File(const std::string data);
