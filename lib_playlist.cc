7264d41b0d6cf45b36ec47f0585f73b6c158a2e4
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-22 18:22:59 +0100

    Added remove, add, setMode functions

--- lib_playlist.cc
+++ lib_playlist.cc
@@ -1,1 +1,30 @@
 #include "lib_playlist.h"
+
+void Playlist::add(Playable *playable) {
+    (this->list).push_back(playable);
+}
+
+void Playlist::add(Playable *playable, int position) {
+    if(position > (this->list).size()){
+        throw PlayerException();
+    }
+    (this->list).insert((this->list).begin() + position, playable);
+}
+
+void Playlist::remove() {
+    if((this->list).empty()){
+        throw PlayerException();
+    }
+    (this->list).pop_back();
+}
+
+void Playlist::remove(int position) {
+    if(position > (this->list).size()){
+        throw PlayerException();
+    }
+    (this->list).erase((this->list).begin() + position);
+}
+
+void Playlist::setMode(Mode *mode) {
+    this->mode= mode;
+}
