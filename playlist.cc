ef6ab85a3ee12712afd8ba1d52f05a6178356dfd
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-22 23:11:48 +0100

    Added order creation

--- playlist.cc
+++ playlist.cc
@@ -2,31 +2,37 @@
 
 
 void Playlist::add(Playable *playable) {
-	(this->list).push_back(playable);
+    (this->list).push_back(playable);
 }
 
 void Playlist::add(Playable *playable, int position) {
-	if (position > (this->list).size()) {
-		throw PlayerException();
-	}
-	(this->list).insert((this->list).begin() + position, playable);
+    if (position > (this->list).size()) {
+        throw PlayerException();
+    }
+    (this->list).insert((this->list).begin() + position, playable);
 }
 
 void Playlist::remove() {
-	if ((this->list).empty()) {
-		throw PlayerException();
-	}
-	(this->list).pop_back();
+    if ((this->list).empty()) {
+        throw PlayerException();
+    }
+    (this->list).pop_back();
 }
 
 void Playlist::remove(int position) {
-	if (position > (this->list).size()) {
-		throw PlayerException();
-	}
-	(this->list).erase((this->list).begin() + position);
+    if (position > (this->list).size()) {
+        throw PlayerException();
+    }
+    (this->list).erase((this->list).begin() + position);
 }
 
 void Playlist::setMode(Mode *newMode) {
-	this->mode = newMode;
+    this->mode = newMode;
+}
+
+void Playlist::play() {
+    std::vector<unsigned int> order = this->mode->getOrder(this->list);
+    for (unsigned int x : order)
+        this->list[x]->play();
 }
 
