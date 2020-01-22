98b9d9a0f13ce25a8bd1fad298638177fc06d82c
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-22 23:25:21 +0100

    Minor changes

--- shuffleMode.cc
+++ shuffleMode.cc
@@ -1,11 +1,10 @@
-#include <random>
 #include <algorithm>
 #include "shuffleMode.h"
 
 std::vector<unsigned int> ShuffleMode::getOrder(std::vector<Playable *> list) {
     std::vector<unsigned int> order(list.size());
     std::iota(std::begin(order), std::end(order), 0); // order is {0, 1, 2, ... list.size() - 1}
-    std::shuffle(order.begin(), order.end(), std::default_random_engine(this->seed));
+    std::shuffle(order.begin(), order.end(), this->engine);
     return order;
 }
 
