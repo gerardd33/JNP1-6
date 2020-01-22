d9825585b4af03e741ef05e7e8d8026f3211c678
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-22 23:09:01 +0100

    Added order creation

--- oddEvenMode.cc
+++ oddEvenMode.cc
@@ -1,12 +1,11 @@
-#ifndef ODDEVENMODE_H
-#define ODDEVENMODE_H
+#include "oddEvenMode.h"
 
+std::vector<unsigned int> OddEvenMode::getOrder(std::vector<Playable *> list) {
+    std::vector<unsigned int> order;
+    for (unsigned int odd = 1; odd < list.size(); odd += 2)
+        order.push_back(odd);
+    for (unsigned int even = 0; even < list.size(); even += 2)
+        order.push_back(even);
 
-#include "mode.h"
-
-class OddEvenMode : public Mode {
-public:
-    std::vector<unsigned int> getOrder(std::vector<Playable *> list) override;
-};
-
-#endif //ODDEVENMODE_H
+    return order;
+}
