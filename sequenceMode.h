1919778012642042cd87f73513c7ff80267b6de5
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-22 23:03:17 +0100

    Added order creation

--- sequenceMode.h
+++ sequenceMode.h
@@ -1,13 +1,11 @@
 #ifndef SEQUENCEMODE_H
 #define SEQUENCEMODE_H
 
-
 #include "mode.h"
 
 class SequenceMode : public Mode {
 public:
-    std::vector<unsigned int> getOrder() override;
+    std::vector<unsigned int> getOrder(std::vector<Playable *> list) override;
 };
-
 
 #endif //SEQUENCEMODE_H
