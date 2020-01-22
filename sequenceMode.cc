28eedb7c556ca2799e98d6767b2988879fa31dae
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-22 23:01:59 +0100

    Added order creation

--- sequenceMode.cc
+++ sequenceMode.cc
@@ -1,1 +1,8 @@
-#include "SequenceMode.h"
+#include <numeric>
+#include "sequenceMode.h"
+
+std::vector<unsigned int> SequenceMode::getOrder(std::vector<Playable *> list) {
+    std::vector<unsigned int> order(list.size());
+    std::iota(std::begin(order), std::end(order), 0); // order is {0, 1, 2, ... list.size() - 1}
+    return order;
+}
