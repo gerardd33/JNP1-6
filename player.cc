daf804b476c7695c96dda01054045a947dad3ed1
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-23 09:37:30 +0100

    Added playlist creation

--- player.cc
+++ player.cc
@@ -1,33 +1,42 @@
 #include "player.h"
 
-
-Playlist* Player::createPlaylist(std::string name) {
-	return nullptr; //TODO cykle
+Playlist *Player::createPlaylist(const std::string &name) {
+    Playlist *playlist = new Playlist(name);
+    auto mode = createSequenceMode();
+    playlist->setMode(mode);
+    playlists.push_back(playlist);
+    return playlist;
 }
 
-Playable* Player::openFile(const File& file) {
-	// TODO: jak z tymi pointerami tu, jakis smart, dynamic_cast?
-	Playable* result;
-	std::string type = file.getType();
+Playable *Player::openFile(const File &file) {
+    // TODO: jak z tymi pointerami tu, jakis smart, dynamic_cast?
+    Playable *result;
+    std::string type = file.getType();
 
-	if (type == "audio")
-		result = new Song(file);
-	else if (type == "video")
-		result = new Movie(file);
-	else throw PlayerException();
-	// TODO: zmien na unsupported type exception
+    if (type == "audio")
+        result = new Song(file);
+    else if (type == "video")
+        result = new Movie(file);
+    else throw PlayerException();
+    // TODO: zmien na unsupported type exception
 
-	return result;
+    return result;
 }
 
-SequenceMode* createSequenceMode() {
-	// ...
+Player::~Player() {
+    for (auto it = begin(this->playlists); it != end(this->playlists); ++it) {
+        delete (*it);
+    }
 }
 
-ShuffleMode* createShuffleMode(int seed) {
-	// ...
+SequenceMode *createSequenceMode() {
+    return new SequenceMode();
 }
 
-OddEvenMode* createOddEvenMode() {
-	// ...
+ShuffleMode *createShuffleMode(int seed) {
+    return new ShuffleMode(seed);
+}
+
+OddEvenMode *createOddEvenMode() {
+    return new OddEvenMode();
 }
