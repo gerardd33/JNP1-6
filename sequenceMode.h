8a5dc3cbdf3ccc70e297b9a7a00776e5c27508d2
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-23 09:26:09 +0100

    Added member functions

--- sequenceMode.h
+++ sequenceMode.h
@@ -1,11 +1,17 @@
-#ifndef SEQUENCEMODE_H
-#define SEQUENCEMODE_H
+#ifndef JNP1_6_SEQUENCEMODE_H
+#define JNP1_6_SEQUENCEMODE_H
 
 #include "mode.h"
 
 class SequenceMode : public Mode {
 public:
     std::vector<unsigned int> getOrder(std::vector<Playable *> list) override;
+
+    ~SequenceMode() override = default;
+
+    SequenceMode() = default;
+
+    SequenceMode* cloneDynamically() override;
 };
 
-#endif //SEQUENCEMODE_H
+#endif //JNP1_6_SEQUENCEMODE_H
