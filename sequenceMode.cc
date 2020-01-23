037c43713a35ab3eecb0888456355ace3f5e7de2
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-23 12:44:17 +0100

    Implemented shared pointers

--- sequenceMode.cc
+++ sequenceMode.cc
@@ -1,12 +1,9 @@
 #include <numeric>
 #include "sequenceMode.h"
 
-std::vector<unsigned int> SequenceMode::getOrder(std::vector<Playable *> list) {
+std::vector<unsigned int> SequenceMode::getOrder(std::vector<std::shared_ptr<Playable>> list) {
     std::vector<unsigned int> order(list.size());
     std::iota(std::begin(order), std::end(order), 0); // order is {0, 1, 2, ... list.size() - 1}
     return order;
 }
 
-SequenceMode *SequenceMode::cloneDynamically() {
-    return new SequenceMode();
-}
