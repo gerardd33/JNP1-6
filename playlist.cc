f723bc4836d2e9279f18eb8992517bfacfcfc677
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-23 18:49:02 +0100

    Switched isPlaylist to containsObject

--- playlist.cc
+++ playlist.cc
@@ -1,19 +1,16 @@
 #include "playlist.h"
+#include "player.h"
 
 void Playlist::add(std::shared_ptr<Playable> playable) {
-    if (playable->isPlaylist()) {
-        std::shared_ptr<Playlist> playlist = std::dynamic_pointer_cast<Playlist>(playable);
-        if (playlist->containsPlaylist(shared_from_this()))
-            throw CycleException();
+    if (playable->containsObject(shared_from_this())) {
+        throw CycleException();
     }
     songList.push_back(playable);
 }
 
 void Playlist::add(std::shared_ptr<Playable> playable, int position) {
-    if (playable->isPlaylist()) {
-        std::shared_ptr<Playlist> playlist = std::dynamic_pointer_cast<Playlist>(playable);
-        if (playlist->containsPlaylist(shared_from_this()))
-            throw CycleException();
+    if (playable->containsObject(shared_from_this())) {
+        throw CycleException();
     }
     if (position > songList.size())
         throw OutOfBoundsException();
@@ -42,25 +39,16 @@ void Playlist::play() {
         songList[x]->play();
 }
 
-// Used to check for cycles
-bool Playlist::containsPlaylist(std::shared_ptr<Playlist> playlist) {
-    bool result = false;
-    songList.begin();
-    for (auto it = songList.begin(); it != songList.end() && !result; ++it) {
-        if ((*it)->isPlaylist()) {
-            if (*it == playlist) {
-                result = true;
-            } else {
-                std::shared_ptr<Playlist> p = std::dynamic_pointer_cast<Playlist>(*it);
-                result = p->containsPlaylist(playlist);
-            }
-        }
-    }
-    return result;
-}
-
 Playlist::Playlist(const std::string& newName) {
     name = newName;
     songList = std::vector<std::shared_ptr<Playable>>();
     mode = createSequenceMode();
+}
+
+bool Playlist::containsObject(std::shared_ptr<Playable> playable) {
+    bool result = (playable.get() == this);
+    for (auto it = songList.begin(); it != songList.end() && !result; ++it) {
+        result = (*it)->containsObject(playable);
+    }
+    return result;
 }
