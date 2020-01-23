c8cb19e7cd094f9a3e12a3296f2b0dcc77bdd496
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-23 09:27:22 +0100

    Added member functions

--- shuffleMode.h
+++ shuffleMode.h
@@ -1,5 +1,5 @@
-#ifndef SHUFFLEMODE_H
-#define SHUFFLEMODE_H
+#ifndef JNP1_6_SHUFFLEMODE_H
+#define JNP1_6_SHUFFLEMODE_H
 
 #include "mode.h"
 #include <random>
@@ -7,11 +7,16 @@
 class ShuffleMode : public Mode {
 private:
     std::default_random_engine engine;
+    int seed;
 public:
-    ShuffleMode(int seed) : engine(std::default_random_engine(seed)) {}
+    explicit ShuffleMode(int seed) : engine(std::default_random_engine(seed)), seed(seed){};
+
+    ~ShuffleMode() override = default;;
+
+    ShuffleMode* cloneDynamically() override;
 
     std::vector<unsigned int> getOrder(std::vector<Playable *> list) override;
 };
 
 
-#endif //SHUFFLEMODE_H
+#endif //JNP1_6_SHUFFLEMODE_H
