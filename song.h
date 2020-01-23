63ddb747c5b32e8efedea74acbd66355fa5c1004
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-23 18:52:03 +0100

    Switched isPlaylist to containsObject

--- song.h
+++ song.h
@@ -1,19 +1,18 @@
 #ifndef JNP1_6_SONG_H
 #define JNP1_6_SONG_H
 
-
 #include "playable.h"
 #include "file.h"
 #include "corruptContentException.h"
 #include <iostream>
+#include <memory>
 
 const std::string SONG_ARTIST_FIELD_NAME = "artist";
 const std::string SONG_TITLE_FIELD_NAME = "title";
 
-
 class Song : public Playable {
 public:
-	bool isPlaylist() override { return false; };
+	bool containsObject(std::shared_ptr<Playable> playable) override { return (playable.get() == this); };
 	void play() override;
 	explicit Song(const File& file);
 
