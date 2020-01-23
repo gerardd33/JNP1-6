d8cc3d51372b0185e9713cbdf746a79f707eccc8
mateuszmazur0 <57603077+mateuszmazur0@users.noreply.github.com>
2020-01-23 10:22:18 +0100

    Implemented PlayerError

--- playerException.h
+++ playerException.h
@@ -5,7 +5,9 @@
 
 class PlayerException : public std::exception {
 public:
-	// TODO: jakis overriding tego what itp
+    [[nodiscard]] const char* what() const noexcept override {
+        return "Player error.";
+    }
 };
 
 #endif //JNP1_6_PLAYEREXCEPTION_H
